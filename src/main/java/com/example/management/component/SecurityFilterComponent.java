022ae1682a73c2c358b2719a4ca2cef7868dcf1f
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-04-19 20:16:58 -0300

    Desenvolvido --     // valida o token e recupera o subject , após verificação o usuário é autenticado
    Removido espaços do Berar prevenindo bug

--- src/main/java/com/example/management/component/SecurityFilterComponent.java
+++ src/main/java/com/example/management/component/SecurityFilterComponent.java
@@ -1,13 +1,19 @@
 package com.example.management.component;
 
+import com.example.management.repository.login.UserRepository;
+import com.example.management.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.Security;
 
 //Filter que realiza a verificação de todas as requisições
 //será executada apenas uma vez por requisição
@@ -15,23 +21,42 @@ import java.io.IOException;
 @Component
 public class SecurityFilterComponent extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recoverToken(request);
+
+        // valida o token e recupera o subject
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            // se chegou até essa etapa o token esta valido - necessário passar para o spring que o usuário esta autenticado
+            var user = userRepository.findByLogin(subject);
+
+            //força a autenticação
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+
         filterChain.doFilter(request, response);
 
-
     }
+
 
     // recupera o token que esta sendo enviado via header
     private String recoverToken(HttpServletRequest request) {
 
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw new RuntimeException("token jwt not received");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "").trim();
         }
-        return authorizationHeader.replace("Bearer" , "");
+        return null;
     }
+
 }
